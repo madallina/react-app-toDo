8ff3df8cb54a3895f32b09ae85a88d342fcdf13a
madallina <goncearucmadalina@yahoo.com>
2024-10-26 13:23:11 +0300

    feat: functions moveTaskUp & moveTaskDown

--- src/ToDoItem.jsx
+++ src/ToDoItem.jsx
@@ -1,10 +1,12 @@
 
-function ToDoItem({task,index,deleteTask}){
+function ToDoItem({task,index,deleteTask, moveTaskUp,moveTaskDown}){
 return (
     <div>
         <li>
             <span>{task}</span>
             <button onClick={()=>deleteTask(index)}>Delete</button>
+            <button onClick={()=>moveTaskUp(index)}>☝</button>
+            <button onClick={()=>moveTaskDown(index)}>👇</button>
         </li>
     </div>
 )
--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -14,11 +14,21 @@ function deleteTask(index){
 const updatedTasks = tasks.filter((el,ind)=> ind!==index);
 setTasks(updatedTasks);
 }
-function moveTaskUp(){
 
+function moveTask(index , direction){
+    const newIndex = index+direction;
+    if(newIndex >= 0 && newIndex < tasks.length){
+        const updatedTasks=[...tasks];
+        [updatedTasks[index],updatedTasks[newIndex]]=
+        [updatedTasks[newIndex], updatedTasks[index]];
+        setTasks(updatedTasks);
+    }
 }
-function moveTaskDown(){
-
+function moveTaskUp(index){
+    moveTask(index,-1);
+}
+function moveTaskDown(index){
+    moveTask(index,1);
 }
 return (
 <div className = "to-do-list">
@@ -29,7 +39,9 @@ return (
 key={index}
 task={task}
 index={index}
-deleteTask={deleteTask} />
+deleteTask={deleteTask}
+moveTaskUp={moveTaskUp}
+moveTaskDown={moveTaskDown} />
 
     ))}
 </ol>
